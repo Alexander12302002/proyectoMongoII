6bb465a7930eb6014f251955a6ead546b6828fc9
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-07-27 17:36:56 -0500

    feat: :sparkles: caso de uso #1 API para Listar Películas

--- js/module/peliculas.js
+++ js/module/peliculas.js
@@ -6,7 +6,7 @@ export class peliculas extends Connect{
             return peliculas.instance;
         }
         super();
-        this.collection = this.db.collection("peliculas");
+        this.collection = this.db.collection("pelicula");
         peliculas.instance = this;
         return this;
     }
@@ -14,5 +14,49 @@ export class peliculas extends Connect{
     async getAllPeliculas(){
         let res = await this.collection.find({},{}).toArray();
         return res
+    }
+
+    //Permitir la consulta de todas las películas disponibles en el catálogo, 
+    //con detalles como título, género, duración y horarios de proyección.
+    async getAllPeliculasConLugarYDuracion(){
+        try{
+            let res = await this.collection.aggregate([
+                {
+                    $lookup: {
+                      from: "funcion",
+                      localField: "_id",
+                      foreignField: "id_pelicula",
+                      as: "funcion"
+                    }
+                  },
+                  {
+                    $unwind: "$funcion"
+                  },
+                  {
+                    $lookup: {
+                      from: "lugar",
+                      localField: "funcion.id_lugar",
+                      foreignField: "_id",
+                      as: "lugar"
+                    }
+                  },
+                  {
+                    $unwind: "$lugar"
+                  },
+                  {
+                    $project: {
+                      _id: 0,
+                      "lugar.nombre": 1,
+                      "funcion.fecha_hora_inicio": 1,
+                      "funcion.fecha_hora_fin": 1,
+                      titulo: 1,
+                      duracion: 1
+                    }
+                  }
+            ]).toArray()
+            return res
+        } catch (error){
+            console.error('Error al insertar al realizar la consulta', error);
+        }
     }
 }
