8c06a2f7c427c122bd41e6ae0beb16595a0c6567
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-07-28 15:49:53 -0500

    feat: :sparkles: caso de uso #5 API para Reservar Asientos

---
+++ js/module/funciones.js
@@ -0,0 +1,23 @@
+import { Connect } from "../../helpers/db/connect.js";
+import { ObjectId } from "mongodb";
+
+export class funciones extends Connect{
+    constructor(){
+        if(typeof funciones.instance === "object") {
+            return funciones.instance;
+        }
+        super();
+        this.collection = this.db.collection("funcion");
+        funciones.instance = this;
+        return this;
+    }
+
+    async getFuncion(id_funcion){
+        try{
+            let res = this.collection.find({_id: new ObjectId(id_funcion)},{}).toArray()
+            return res
+        } catch (error){
+            console.log("No se encuentra la funcion verifique el id", error)
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { peliculas } from "./js/module/peliculas.js";
 import { boletas } from "./js/module/boletas.js";
+import { reservar } from "./js/module/reservarAsientos.js";
 
 /**
  * @file main.js
  * @description Archivo principal para interactuar con la API de compra de boletos y consulta de películas.
  */
-let mongo = new boletas();
+let mongo = new reservar();
 
 // ! Consultar todas las películas disponibles en el catálogo con detalles como título, género, duración y horarios de proyección
 // console.log(await mongo.getAllPeliculasConLugarYDuracion());
@@ -32,10 +33,29 @@ let mongo = new boletas();
 // ! Permitir la compra de boletos para una película específica, incluyendo la selección de la fecha y la hora de la proyección.
 //console.log(await mongo.setComprarBoletas(JsonBoletos));
 
-    /**
-     * @description Permitir la consulta de la disponibilidad de asientos.
-     * @param {string} id_asiento - ID del asiento.
-     * @returns {Promise<Object>} Resultado de la verificacion del boleto.
-     */
+/**
+ * @description Permitir la consulta de la disponibilidad de asientos.
+ * @param {string} id_asiento - ID del asiento.
+ * @returns {Promise<Object>} Resultado de la verificacion del boleto.
+ */
 // ! Permitir la consulta de la disponibilidad de asientos en una sala para una proyección específica.
 //console.log(await mongo.getVerificacionAsiento(id_asiento));
+
+
+/**
+ * @description Permitir la selección y reserva de asientos para una proyección específica.
+ * @param {Object} JsonReserva - Objeto que contiene los datos necesarios para la reserva del asiento.
+ * @param {string} JsonReserva.id_cliente - ID del cliente.
+ * @param {string} JsonReserva.id_asiento - ID del asiento.
+ * @param {string} JsonReserva.id_funcion - ID de la funcion.
+ * @param {string} JsonReserva.fecha_reserva - Fecha de la reserva
+ * @returns {Promise<Object>} Resultado de la compra del boleto.
+ */
+const JsonReserva = {
+    id_cliente: '66a66409c95fdf47d22d694e',
+    id_asiento: '66a6550fc95fdf47d22d6942',
+    id_funcion: '66a4792522edecf73bf6e6db',
+    fecha_reserva: '2024-07-01T18:00:00.000+00:00'
+};
+// ! Permitir la selección y reserva de asientos para una proyección específica.
+console.log(await mongo.setReservar(JsonReserva))
