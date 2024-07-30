36ba5286ead93377ec49b9d7cb60e7331d8acddd
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-07-29 23:24:15 -0500

    feat: :sparkles: caso de uso #8 API para Crear Usuario

--- js/module/clientes.js
+++ js/module/clientes.js
@@ -20,4 +20,56 @@ export class clientes extends Connect{
             console.log("No se encuentra el movimiento verifique el id", error)
         }
     }
+
+
+
+    /**
+     * Crea un nuevo usuario en la base de datos y en el sistema de autenticación de MongoDB.
+     * 
+     * @param {Object} usuario - Objeto que contiene la información del usuario a crear.
+     * @param {string} usuario.nombre - Nombre del usuario.
+     * @param {string} usuario.apellido - Apellido del usuario.
+     * @param {string} usuario.nick - Nickname o nombre de usuario.
+     * @param {string} usuario.email - Correo electrónico del usuario.
+     * @param {string} usuario.telefono - Número de teléfono del usuario.
+     * @param {string} usuario.id_tipo_de_categoria - ID de la categoría del usuario.
+     * @param {string} usuario.cedula - Cédula o documento de identidad del usuario.
+     * @param {string} usuario.rol - Rol del usuario en la base de datos de MongoDB.
+     * @returns {Object} Mensaje de éxito o error y el usuario creado.
+     */
+    async crearUsuario(usuario) {
+         // Desestructurar los campos necesarios del objeto 'usuario'   
+        let { nombre, apellido, nick, email, telefono, id_tipo_de_categoria: id, cedula, rol } = usuario;
+         // Verificar si ya existe un usuario con el mismo nick, cedula o email
+        const condicion = await this.collection.find({
+          $or: [
+            { nick: nick },
+            { cedula: cedula },
+            { email: email }
+          ]
+        }).toArray();
+         // Si el usuario ya existe, retornar un mensaje indicando el conflicto
+        if (condicion.length) return { mensaje: "El usuario ya existe", usuario };
+        // Insertar el nuevo usuario en la colección de usuarios
+        const res = await this.collection.insertOne({
+            id_tipo_de_categoria: new ObjectId(id),
+            nombre,
+            apellido,
+            nick,
+            email,
+            telefono,
+            cedula,
+            rol
+        });
+        // Crear el usuario en el sistema de autenticación de MongoDB
+        const crearUsuario = await this.db.command({
+            createUser: nick,
+            pwd: `${cedula}`,
+            roles: [
+                {role: rol, db: process.env.MONGO_DB}
+            ]
+        });
+        await this.close();
+        return { mensaje: "El usuario fue creado", usuario: res}
+    }
 }
--- main.js
+++ main.js
@@ -2,12 +2,13 @@ import { peliculas } from "./js/module/peliculas.js";
 import { boletas } from "./js/module/boletas.js";
 import { reservar } from "./js/module/reservarAsientos.js";
 import { tarjetas } from "./js/module/tarjetas.js";
+import { clientes } from "./js/module/clientes.js";
 
 /**
  * @file main.js
  * @description Archivo principal para interactuar con la API de compra de boletos y consulta de películas.
  */
-let mongo = new tarjetas();
+let mongo = new clientes();
 
 //Selección de Películas
 
@@ -72,3 +73,33 @@ const JsonReserva = {
 
 // ! Permitir la verificación de la validez de una tarjeta VIP durante el proceso de compra.
 //console.log(await mongo.getValidarTarjetas("66a66409c95fdf47d22d694b"))
+
+//Roles Definidos
+
+/**
+ * Crea un nuevo usuario en la base de datos y en el sistema de autenticación de MongoDB.
+ * 
+ * @param {Object} usuario - Objeto que contiene la información del usuario a crear.
+ * @param {string} nombre - Nombre del usuario.
+ * @param {string} apellido - Apellido del usuario.
+ * @param {string} nick - Nickname o nombre de usuario.
+ * @param {string} email - Correo electrónico del usuario.
+ * @param {string} telefono - Número de teléfono del usuario.
+ * @param {string} id_tipo_de_categoria - ID de la categoría del usuario.
+ * @param {string} cedula - Cédula o documento de identidad del usuario.
+ * @param {string} rol - Rol del usuario en la base de datos de MongoDB.
+ * @returns {Object} Mensaje de éxito o error y el usuario creado.
+ */
+const data = {
+    nombre: "Jaime",
+    apellido: "Alexander",
+    nick: "Aki1130",
+    email: "Aki1130@example.com",
+    telefono: "555-5678",
+    id_tipo_de_categoria: "66a663a914eddb58d943e299",
+    cedula: "98765432",
+    rol: "usuarioVip"
+};
+
+// !  Permitir la creación de nuevos usuarios en el sistema, asignando roles y privilegios específicos (usuario estándar, usuario VIP o administrador).
+//console.log(await mongo.crearUsuario(data))
